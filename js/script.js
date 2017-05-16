b33657f47c379a33c8c142d549cecdae3a1e3651
aaliaga <a.aliaga.val@gmail.com>
2017-05-16 12:58:33 -0400

    sin la ultima diapo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,54 @@
 function agregar(){
+
+	//Paso 1
+	var tareas = document.getElementById("tarea").value;
+	document.getElementById("tarea").value="";
+
+	//paso2
+	var cont = document.getElementById("contenedor");
+
+	//paso3
+	var nuevasTareas = document.createElement("div");
+	var textoNuevaTarea = document.createTextNode(tareas);
+	var elementoContenedor = document.createElement("span");
+	
+	//paso4
+	elementoContenedor.appendChild(textoNuevaTarea);
+	nuevasTareas.appendChild(elementoContenedor);
+	cont.appendChild(nuevasTareas);
+
+	//paso5
+	var chck = document.createElement("input");
+	chck.type = "checkbox";
+	chck.setAttribute("class", "check");
+	var basura = document.createElement("span");
+	basura.classList.add("fa", "fa-trash-o");
+	var cora = document.createElement("span");
+	cora.classList.add("fa", "fa-heart");
+
+	//paso6
+	nuevasTareas.appendChild(chck);
+	nuevasTareas.appendChild(basura);
+	nuevasTareas.appendChild(cora);
+
+	//paso7
+	chck.addEventListener("click", function (){
+		elementoContenedor.classList.toogle("tachado");
+	})
+
+	//paso8
+	basura.addEventListener("click", function(){
+		cont.removeChild(nuevasTareas);
+	})
+
+	cora.addEventListener("click", function(){
+		cora.classList.toogle("red");
+	})
+
+	if(tareas == null || tareas.length == 0){
+		alert("¡Error! Debe ingresar tarea");
+		return false;
+	}
 	
 }
 
